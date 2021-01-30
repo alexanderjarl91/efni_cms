4e06e7f0414e1151bee97a8dc0c84468008b27c5
Johann <johannth88@gmail.com>
2021-01-30 19:52:54 +0000

    user access check in collection page

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -3,34 +3,43 @@ import queryString from 'query-string';
 import "./styles/collection.css";
 import NewEntry from "./NewEntry";
 import EditEntry from "./EditEntry";
-import { DataContext } from "../context";
+import { AuthContext, DataContext } from "../context";
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
 export default function Collection(props) {
+  // const { products, setProducts } = useContext(DataContext);
+  const { user } = useContext(AuthContext);
+  const { userData } = useContext(DataContext);
   const [addMode, setAddMode] = useState(false);
   const [currEndPoint, setCurrEndPoint] = useState('');
   const [currCollection, setCurrCollection] = useState([]);
-  const { products, setProducts } = useContext(DataContext);
   const [editMode, setEditMode] = useState(false);
   const [productToEdit, setProductToEdit] = useState([]);
+  const [allowAccess, setAllowAccess] = useState([false]);
 
   useEffect(() => {
-    // Get the collection from the url
     const collectionQs = queryString.parse(window.location.search);
-    setCurrEndPoint(collectionQs.name);
-
-    const getCollection = async () => {
-      try {
-        const response = await fetch(`https://efni-api.herokuapp.com/${collectionQs.name}`);
-        const data = await response.json();
-        setCurrCollection(data);
-      } catch(err) {
-        console.log(err)
+    const foundUser = userData.find(x => x.email === user.email); // Get the current user object that contains the data access
+    // If the user has the requeststring in his access array we allow him access
+    if(foundUser.access.includes(collectionQs.name)) {
+      // Get the collection from the url
+      setCurrEndPoint(collectionQs.name);
+      setAllowAccess(true);
+  
+      const getCollection = async () => {
+        try {
+          const response = await fetch(`https://efni-api.herokuapp.com/${collectionQs.name}`);
+          const data = await response.json();
+          setCurrCollection(data);
+        } catch(err) {
+          console.log(err)
+        }
       }
+      getCollection();
+    } else {
+      setAllowAccess(false);
     }
-
-    getCollection();
   }, [window.location.search]);
 
   const toggleAddMode = () => {
@@ -72,53 +81,58 @@ export default function Collection(props) {
 
   return (
     <>
-      {addMode ? (
-        <NewEntry setProducts={setCurrCollection} currEndPoint={currEndPoint} toggleAddMode={toggleAddMode} />
-      ) : null}
-      <div>
-        <h1>{currEndPoint}</h1>
-
-        <div className="collection__entryHeader">
-          <h4>entries:</h4>
-          <button onClick={toggleAddMode}>add new entry</button>
-        </div>
-
+      {allowAccess ? (
+        <>
+        {addMode ? (
+          <NewEntry setProducts={setCurrCollection} currEndPoint={currEndPoint} toggleAddMode={toggleAddMode} />
+        ) : null}
         <div>
-          <div className="collection__titles">
-            <h4>Product Name</h4>
-            <h4>product price</h4>
-            <h4>product image</h4>
-            <h4>on sale?</h4>
-            <h4>description</h4>
-            <h4></h4>
-            <h4></h4>
+          <h1>{currEndPoint}</h1>
 
-            {currCollection.map((product) => (
-              <React.Fragment key={product._id}>
-                <p>{product.productName}</p>
-                <p>{product.productPrice}</p>
-                <a
-                  className="collection__imageLink"
-                  href={product.productImg}
-                  target="blank"
-                >
-                  Link to image
-                </a>
-                <p>{product.productOnSale ? "true" : "false"}</p>
-                <p>{product.productDescription}</p>
-                <EditIcon className="collection__actionIcons" onClick={() => toggleEdit(product._id)}/>
-                <DeleteIcon className="collection__actionIcons" onClick={() => handleDelete(product._id)}/>
-                {editMode ? (
-                    <EditEntry setEditMode={setEditMode} productToEdit={productToEdit}/>
-                ) : null}
-              </React.Fragment>
+          <div className="collection__entryHeader">
+            <h4>entries:</h4>
+            <button onClick={toggleAddMode}>add new entry</button>
+          </div>
 
-            ))}
-             
-           
+          <div>
+            <div className="collection__titles">
+              <h4>Product Name</h4>
+              <h4>product price</h4>
+              <h4>product image</h4>
+              <h4>on sale?</h4>
+              <h4>description</h4>
+              <h4></h4>
+              <h4></h4>
+
+              {currCollection.map((product) => (
+                <React.Fragment key={product._id}>
+                  <p>{product.productName}</p>
+                  <p>{product.productPrice}</p>
+                  <a
+                    className="collection__imageLink"
+                    href={product.productImg}
+                    target="blank"
+                  >
+                    Link to image
+                  </a>
+                  <p>{product.productOnSale ? "true" : "false"}</p>
+                  <p>{product.productDescription}</p>
+                  <EditIcon className="collection__actionIcons" onClick={() => toggleEdit(product._id)}/>
+                  <DeleteIcon className="collection__actionIcons" onClick={() => handleDelete(product._id)}/>
+                  {editMode ? (
+                      <EditEntry setEditMode={setEditMode} productToEdit={productToEdit}/>
+                  ) : null}
+                </React.Fragment>
+
+              ))}
+              
+            
+            </div>
           </div>
         </div>
-      </div>
+        </>
+        ) : (<div>No Access to this collection</div>)
+      }
     </>
   );
 }
