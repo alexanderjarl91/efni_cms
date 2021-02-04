414e12a0219574154a9b6dc5b80f69440f243791
Alexander <alexanderjarl91@gmail.com>
2021-02-04 21:18:11 +0000

    firebase.js fixed

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB_aN3ousoDrcg6VmXR6DzKe9SOCZvk56Q",
-//   authDomain: "efni-cms.firebaseapp.com",
-//   projectId: "efni-cms",
-//   storageBucket: "efni-cms.appspot.com",
-//   messagingSenderId: "47309262747",
-//   appId: "1:47309262747:web:3439cc6d4a9be2c4051b3e",
-//   measurementId: "G-BKG324Y7C0",
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
